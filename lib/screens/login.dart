7c8cc8b71bb8d650ab783fa18db7b1c56a9f5fc0
Mr-Ashitey <aamarh17@gmail.com>
2023-11-16 12:10:57 +0000

    refactor login to look more like UI

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,67 +1,66 @@
 import 'package:flutter/material.dart';
 import 'package:qg_stock_take_app/constants/colors.dart';
+import 'package:qg_stock_take_app/constants/size_config.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    ScreenSize().init(context);
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: primaryColor,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 100),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const TextField(
-              decoration: InputDecoration(
-                  hintText: 'Phone Number',
-                  hintStyle: TextStyle(
-                    fontSize: 21,
-                    color: Colors.black38,
-                  ),
-                  filled: true,
-                  fillColor: Colors.white,
-                  border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white))),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const TextField(
-              decoration: InputDecoration(
-                  hintText: 'Station Code',
-                  hintStyle: TextStyle(
-                    fontSize: 21,
-                    color: Colors.black38,
-                  ),
-                  filled: true,
-                  fillColor: Colors.white,
-                  border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white))),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            SizedBox(
-              height: 50,
-              width: MediaQuery.sizeOf(context).width,
-              child: ElevatedButton(
+      body: Center(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.symmetric(
+              vertical: 0, horizontal: MediaQuery.sizeOf(context).width * 0.1),
+          child: Column(
+            children: [
+              TextField(
+                onTapOutside: (event) => FocusScope.of(context).unfocus(),
+                decoration: InputDecoration(
+                    hintText: 'Phone Number',
+                    hintStyle: TextStyle(
+                      fontSize: getProportionateScreenHeight(21),
+                      color: Colors.black38,
+                    ),
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: InputBorder.none),
+              ),
+              SizedBox(height: getProportionateScreenHeight(10)),
+              TextField(
+                onTapOutside: (event) => FocusScope.of(context).unfocus(),
+                decoration: InputDecoration(
+                    hintText: 'Station Code',
+                    hintStyle: TextStyle(
+                      fontSize: getProportionateScreenHeight(21),
+                      color: Colors.black38,
+                    ),
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: InputBorder.none),
+              ),
+              SizedBox(height: getProportionateScreenHeight(20)),
+              ElevatedButton(
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.amberAccent,
+                  fixedSize: Size(MediaQuery.sizeOf(context).width,
+                      getProportionateScreenHeight(40)),
                 ),
-                child: const Text(
+                child: Text(
                   'LOGIN',
                   style: TextStyle(
-                    color: Color.fromARGB(255, 236, 34, 39),
-                    fontSize: 25,
-                    fontWeight: FontWeight.w300,
+                    color: primaryColor,
+                    fontSize: getProportionateScreenHeight(18),
+                    fontWeight: FontWeight.w400,
                   ),
                 ),
                 onPressed: () {},
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
