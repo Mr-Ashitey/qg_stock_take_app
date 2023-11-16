3dfa12e8b904b5284eedd6e9a5ba08e2de3611c9
Khaleb165 <calebagyei165@gmail.com>
2023-11-16 16:24:43 +0000

    add and remove colors

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
 Color primaryColor = const Color(0xFFEC2227);
+Color primaryDarkColor = const Color(0xFFC11825);
 Color colorYellow = Colors.amberAccent;
 Color colorGrey = const Color(0xFFADADAD);
 Color colorWhite = const Color(0xFFFAFAFA);
 Color primaryColor2 = const Color(0xFFEE534F);
 Color primaryColor3 = const Color(0xFFE57373);
-Color primaryColor4 = const Color(0xFFE07C7C);
+// Color primaryColor4 = const Color(0xFFE07C7C);
 Color colorDeepBlue1 = const Color(0xFF062463);
 Color colorLightBlue = const Color(0xFF0099CB);
 Color colorDeepGrey = const Color(0xFF4F4F4F);
