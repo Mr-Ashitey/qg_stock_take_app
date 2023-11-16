3c5cf4d33992cecf9f82cc89b6a36d7a52f261be
Khaleb165 <calebagyei165@gmail.com>
2023-11-16 12:44:31 +0000

    added more colors

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,3 +1,14 @@
 import 'package:flutter/material.dart';
 
 Color primaryColor = const Color(0xFFEC2227);
+Color colorYellow = Colors.amberAccent;
+Color colorGrey = const Color(0xFFADADAD);
+Color colorWhite = const Color(0xFFFAFAFA);
+Color primaryColor2 = const Color(0xFFEE534F);
+Color primaryColor3 = const Color(0xFFE57373);
+Color primaryColor4 = const Color(0xFFE07C7C);
+Color colorDeepBlue1 = const Color(0xFF062463);
+Color colorLightBlue = const Color(0xFF0099CB);
+Color colorDeepGrey = const Color(0xFF4F4F4F);
+Color colorDeepBlue2 = const Color(0xFF031A44);
+Color colorLightBlue2 = const Color(0xFF1564C0);
