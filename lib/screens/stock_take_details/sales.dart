16089d403a91d6f5ef8b30349a98b918b07bf5b2
Khaleb165 <calebagyei165@gmail.com>
2023-11-17 11:59:00 +0000

    add other cards to the sales screen

--- lib/screens/stock_take_details/sales.dart
+++ lib/screens/stock_take_details/sales.dart
@@ -8,6 +8,7 @@ class SalesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: colorWhite,
       appBar: AppBar(
         toolbarHeight: getProportionateScreenHeight(60),
         backgroundColor: primaryColor,
@@ -33,19 +34,15 @@ class SalesScreen extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              SizedBox(
-                height: getProportionateScreenHeight(10),
-              ),
+              SizedBox(height: getProportionateScreenHeight(10)),
               Text(
                 'Stock for: Dec 13,2019',
                 style: TextStyle(
                     fontSize: getProportionateScreenHeight(19),
                     fontWeight: FontWeight.bold),
               ),
-              SizedBox(
-                height: getProportionateScreenHeight(15),
-              ),
-              buildCard(
+              SizedBox(height: getProportionateScreenHeight(15)),
+              buildSaleCard(
                 'Power Diesel',
                 0.00,
                 0.00,
@@ -55,6 +52,67 @@ class SalesScreen extends StatelessWidget {
                 0.00,
                 () {},
               ),
+              SizedBox(height: getProportionateScreenHeight(15)),
+              buildCardDamsa(
+                'DAMSA(Power Diesel)',
+                'DT1',
+                0,
+                () {},
+              ),
+              SizedBox(height: getProportionateScreenHeight(15)),
+              buildSaleCard(
+                'Power Super',
+                0.00,
+                0.00,
+                0.00,
+                0.00,
+                0.00,
+                0.00,
+                () {},
+              ),
+              SizedBox(height: getProportionateScreenHeight(15)),
+              buildCardDamsa(
+                'DAMSA(Power Super)',
+                'ST1',
+                0,
+                () {},
+              ),
+              SizedBox(height: getProportionateScreenHeight(15)),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  elevation: 0,
+                  padding: const EdgeInsets.only(top: 20, left: 10, bottom: 20),
+                  fixedSize:
+                      Size(double.maxFinite, getProportionateScreenHeight(140)),
+                  backgroundColor: colorLightBlue2,
+                ),
+                child: Column(
+                  mainAxisAlignment: mainSpaceBetween,
+                  children: [
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Icon(
+                        Icons.help_outline,
+                        color: primaryDarkColor,
+                      ),
+                    ),
+                    const Spacer(),
+                    Center(
+                      child: Text(
+                        'Stock Loss',
+                        style: TextStyle(
+                          fontSize: getProportionateScreenHeight(17),
+                          color: colorWhite,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                    const Spacer(),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 40),
             ],
           ),
         ),
@@ -62,7 +120,7 @@ class SalesScreen extends StatelessWidget {
     );
   }
 
-  ElevatedButton buildCard(
+  ElevatedButton buildSaleCard(
     String title,
     double grandTotal,
     double credit,
@@ -113,6 +171,47 @@ class SalesScreen extends StatelessWidget {
             'Genset Sale: $gensetSale',
           ),
           Text('Fuel Coupon: $fuelCoupon')
+        ],
+      ),
+    );
+  }
+
+  ElevatedButton buildCardDamsa(
+    String title,
+    String variationTitle,
+    double variation,
+    VoidCallback onPressed,
+  ) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        elevation: 0,
+        padding: const EdgeInsets.only(top: 20, left: 10, bottom: 20),
+        backgroundColor: colorDeepBlue2,
+      ),
+      child: Column(
+        // mainAxisAlignment: mainSpaceBetween,
+        children: [
+          Align(
+            alignment: Alignment.topLeft,
+            child: Icon(
+              Icons.help_outline,
+              color: primaryDarkColor,
+            ),
+          ),
+          Center(
+            child: Text(
+              title,
+              style: TextStyle(
+                fontSize: getProportionateScreenHeight(17),
+                color: colorWhite,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+          Text('Variation for $variationTitle: $variation'),
+          Text('Variation for $variationTitle: $variation'),
+          SizedBox(height: getProportionateScreenHeight(35)),
         ],
       ),
     );
