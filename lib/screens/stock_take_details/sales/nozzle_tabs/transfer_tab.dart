4f5885cb70bd4c2f1fee506d52326f34c0c67927
khaleb165 <calebagyei165@gmail.com>
2023-11-20 20:37:46 +0000

    change transfer page to stateful widget and add AutomaticKeepAliveMixin

--- lib/screens/stock_take_details/sales/nozzle_tabs/transfer_tab.dart
+++ lib/screens/stock_take_details/sales/nozzle_tabs/transfer_tab.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 
-class TransferPage extends StatelessWidget {
+class TransferPage extends StatefulWidget {
   const TransferPage({super.key});
 
+  @override
+  State<TransferPage> createState() => _TransferPageState();
+}
+
+class _TransferPageState extends State<TransferPage>
+    with AutomaticKeepAliveClientMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(child: Text('Transfer page')),
     );
   }
+
+  @override
+  bool get wantKeepAlive => true;
 }
