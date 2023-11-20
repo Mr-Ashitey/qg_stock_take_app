bb5aaff034f10a5df7572747f6a3b8d52b3f2f51
khaleb165 <calebagyei165@gmail.com>
2023-11-20 20:39:20 +0000

    change goodsInTransit to stateful widget and add AutomaticKeepAliveMixin

--- lib/screens/stock_take_details/sales/nozzle_tabs/goods_in_transit.dart
+++ lib/screens/stock_take_details/sales/nozzle_tabs/goods_in_transit.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 
-class GoodsInTransit extends StatelessWidget {
+class GoodsInTransit extends StatefulWidget {
   const GoodsInTransit({super.key});
 
+  @override
+  State<GoodsInTransit> createState() => _GoodsInTransitState();
+}
+
+class _GoodsInTransitState extends State<GoodsInTransit>
+    with AutomaticKeepAliveClientMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(child: Text('Goods in transit')),
     );
   }
+
+  @override
+  bool get wantKeepAlive => true;
 }
