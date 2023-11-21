385562915bf6fb74f0f21fbe9f2e1242aa4109ee
khaleb165 <calebagyei165@gmail.com>
2023-11-21 12:49:59 +0000

    implement speedDial for floating actions

--- lib/screens/stock_take_details/sales/nozzle_tabs/goods_in_transit.dart
+++ lib/screens/stock_take_details/sales/nozzle_tabs/goods_in_transit.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:qg_stock_take_app/constants/colors.dart';
 
 class GoodsInTransit extends StatefulWidget {
   const GoodsInTransit({super.key});
@@ -12,10 +14,39 @@ class _GoodsInTransitState extends State<GoodsInTransit>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Goods in transit')),
+      floatingActionButton: buildSpeedDial(),
+      body: const Center(child: Text('Goods in transit')),
     );
   }
 
   @override
   bool get wantKeepAlive => true;
+
+  SpeedDial buildSpeedDial() {
+    return SpeedDial(
+      animatedIcon: AnimatedIcons.menu_close,
+      animatedIconTheme: const IconThemeData(size: 22.0),
+      backgroundColor: colorYellow,
+      visible: true,
+      curve: Curves.bounceIn,
+      children: [
+        SpeedDialChild(
+          child: const Icon(Icons.edit_outlined, color: Colors.white),
+          backgroundColor: colorLightBlue,
+          onTap: () {},
+          label: 'Manual Discharge',
+          labelStyle: TextStyle(color: colorWhite),
+          labelBackgroundColor: Colors.grey[700],
+        ),
+        SpeedDialChild(
+          child: const Icon(Icons.qr_code, color: Colors.white),
+          backgroundColor: colorLightBlue,
+          onTap: () {},
+          label: 'Scan QR code',
+          labelStyle: TextStyle(color: colorWhite),
+          labelBackgroundColor: Colors.grey[700],
+        ),
+      ],
+    );
+  }
 }
