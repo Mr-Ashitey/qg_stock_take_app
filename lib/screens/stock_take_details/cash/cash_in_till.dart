10605f7621132f37641ea9101d095e64d7708ef3
khaleb165 <calebagyei165@gmail.com>
2023-11-22 20:11:35 +0000

    complete cash in till page

--- lib/screens/stock_take_details/cash/cash_in_till.dart
+++ lib/screens/stock_take_details/cash/cash_in_till.dart
@@ -1,10 +1,115 @@
 import 'package:flutter/material.dart';
+import 'package:qg_stock_take_app/constants/colors.dart';
+import 'package:qg_stock_take_app/constants/size_config.dart';
 
-class CashInTill extends StatelessWidget {
+class CashInTill extends StatefulWidget {
   const CashInTill({super.key});
 
   @override
+  State<CashInTill> createState() => _CashInTillState();
+}
+
+class _CashInTillState extends State<CashInTill> {
+  double totalAmount = 0.0;
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: _showDialog,
+        backgroundColor: colorYellow,
+        child: const Icon(
+          Icons.add,
+          color: Colors.black,
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          height: 35,
+          color: Colors.black87,
+          child: Text(
+            'Total Amount: ₵ $totalAmount',
+            style: TextStyle(
+              color: colorWhite,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _showDialog() {
+    double amountRemaining = 0.0;
+
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            surfaceTintColor: Colors.white,
+            scrollable: true,
+            title: Text(
+              'Add Unbanked Sale',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: getProportionateScreenHeight(20),
+              ),
+            ),
+            content: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Column(
+                children: [
+                  getField(
+                    'Date',
+                  ),
+                  SizedBox(height: getProportionateScreenHeight(5)),
+                  getField(
+                    'Amount',
+                  ),
+                  SizedBox(height: getProportionateScreenHeight(30)),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      'Amount Remaining: $amountRemaining',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: getProportionateScreenHeight(13),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: Text(
+                  'CANCEL',
+                  style: TextStyle(color: colorYellow),
+                ),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Text(
+                  'ADD',
+                  style: TextStyle(color: colorYellow),
+                ),
+              ),
+            ],
+          );
+        });
+  }
+
+  TextField getField(String hint) {
+    return TextField(
+      decoration: InputDecoration(
+        hintText: hint,
+        hintStyle: TextStyle(
+          color: Colors.black38,
+          fontSize: getProportionateScreenHeight(15),
+        ),
+        border: const UnderlineInputBorder(),
+      ),
+    );
   }
 }
