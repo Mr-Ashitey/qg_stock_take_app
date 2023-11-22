3b9d3f0bf3248b8292c398fa3f41f3ca1d3b3b34
khaleb165 <calebagyei165@gmail.com>
2023-11-22 13:17:43 +0000

    add cash count tab page

---
+++ lib/screens/stock_take_details/cash/cash_count.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CashCount extends StatelessWidget {
+  const CashCount({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('cash count'),
+      ),
+    );
+  }
+}
