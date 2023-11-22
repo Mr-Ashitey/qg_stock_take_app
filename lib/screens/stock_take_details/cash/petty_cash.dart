ab0756f360c2dbca49945f5e781ff0ce7867aa4e
khaleb165 <calebagyei165@gmail.com>
2023-11-22 13:19:40 +0000

    add petty cash tab page

---
+++ lib/screens/stock_take_details/cash/petty_cash.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PettyCash extends StatelessWidget {
+  const PettyCash({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
