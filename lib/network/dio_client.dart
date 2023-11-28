27133b3e9f17930bd73d5a50bcfe66c7a102d284
Mr-Ashitey <aamarh17@gmail.com>
2023-11-28 19:48:31 +0000

    add custom dio network client

---
+++ lib/network/dio_client.dart
@@ -0,0 +1,68 @@
+import 'package:awesome_dio_interceptor/awesome_dio_interceptor.dart';
+import 'package:dio/dio.dart';
+import 'package:qg_stock_take_app/network/interceptors/token_interceptor.dart';
+
+import 'dio_exceptions.dart';
+
+class DioClient {
+  static final DioClient _instance = DioClient.internal();
+
+  factory DioClient() => _instance;
+
+  static late Dio _dio;
+
+  DioClient.internal();
+
+  Future<void> initDioClient() async {
+    final tokenInterceptor = TokenInterceptor();
+    tokenInterceptor.setDioInstance(_dio);
+    _dio = Dio(
+      BaseOptions(
+        baseUrl: '',
+      ),
+    );
+
+    _dio.interceptors.addAll([
+      AwesomeDioInterceptor(),
+      tokenInterceptor,
+    ]);
+  }
+
+  // get endpoint
+  Future get(String endpoint,
+      [Map<String, dynamic>? queryParameters, String token = '']) async {
+    Response response;
+
+    try {
+      response = await _dio.get(endpoint, queryParameters: queryParameters);
+      return response.data;
+    } on DioException catch (e) {
+      final errorMessage = DioExceptions.fromDioError(e).toString();
+      throw (errorMessage);
+    }
+  }
+
+  // post endpoint
+  Future post(String endpoint, [Map<String, dynamic>? body]) async {
+    Response response;
+
+    try {
+      response = await _dio.post(endpoint, data: body);
+
+      return response.data;
+    } on DioException catch (e) {
+      final errorMessage = DioExceptions.fromDioError(e).toString();
+      throw (errorMessage);
+    }
+  }
+
+  // patch endpoint
+  Future<void> patch(String endpoint, [Map<String, dynamic>? body]) async {
+    try {
+      await _dio.patch(endpoint, data: body);
+    } on DioException catch (e) {
+      final errorMessage = DioExceptions.fromDioError(e).toString();
+      throw (errorMessage);
+    }
+  }
+}
