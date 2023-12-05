060ec6e7e99985ad65dbae62f798ab71305a26e6
Mr-Ashitey <aamarh17@gmail.com>
2023-12-05 11:24:04 +0000

    add constants variables

---
+++ lib/constants/constants.dart
@@ -0,0 +1,24 @@
+class Constants {
+  static const String appUrl = "https://pmob.cardinalapps.net/";
+  static const int bank = 75;
+  static const int bankCash = 0;
+  static const int cashCount = 0;
+  static const int cashSAle = 1;
+  static const int cheque = 1;
+  static const int coupon = 5;
+  static const int creditSale = 2;
+  static const int damsa = 987;
+  static const int expenses = 163;
+  static const int genset = 3;
+  static const String jwtExpired = "JWT expired";
+  static const int liubesCash = 2;
+  static const String meterSale = "meter_sale";
+  static const int nozzleTransfer = 2;
+  static const int noRecord = 666;
+  static const int pettyCash = 1;
+  static const String qrCodeString = "qr_code";
+  static const String selectDate = "SELECT DATE";
+  static const int stockLoss = 777;
+  static const int underGroupTransfer = 1;
+  static const int wholesale = 4;
+}
