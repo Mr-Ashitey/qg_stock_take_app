e143ce8c372bd9d6f39cf9914f796344f04aedb5
Mr-Ashitey <aamarh17@gmail.com>
2023-12-05 11:29:17 +0000

    add text controllers and login logic

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:provider/provider.dart';
 import 'package:qg_stock_take_app/constants/colors.dart';
 import 'package:qg_stock_take_app/constants/keyboard.dart';
 import 'package:qg_stock_take_app/constants/size_config.dart';
+import 'package:qg_stock_take_app/extensions/snackbar_alert_extension.dart';
 import 'package:qg_stock_take_app/screens/select_station.dart';
 
-class LoginScreen extends StatelessWidget {
+import '../providers/login_provider.dart';
+import '../util/base_provider.dart';
+import '../util/custom_progress_indicator.dart';
+
+class LoginScreen extends HookWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final phoneNumberCtrl = useTextEditingController();
+    final codeCtrl = useTextEditingController();
+    final isLoading =
+        context.watch<LoginProvider>().notifierState == NotifierState.loading;
+
     ScreenSize().init(context);
+
     return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: primaryColor,
@@ -21,6 +34,7 @@ class LoginScreen extends StatelessWidget {
             children: [
               TextField(
                 maxLength: 10,
+                controller: phoneNumberCtrl,
                 keyboardType: TextInputType.phone,
                 onTapOutside: (event) => hideKeyboard(context),
                 decoration: InputDecoration(
@@ -37,6 +51,7 @@ class LoginScreen extends StatelessWidget {
               SizedBox(height: getProportionateScreenHeight(10)),
               TextField(
                 maxLength: 4,
+                controller: codeCtrl,
                 keyboardType: TextInputType.phone,
                 onTapOutside: (event) => hideKeyboard(context),
                 decoration: InputDecoration(
@@ -57,19 +72,38 @@ class LoginScreen extends StatelessWidget {
                   fixedSize: Size(MediaQuery.sizeOf(context).width,
                       getProportionateScreenHeight(40)),
                 ),
-                child: Text(
-                  'LOGIN',
-                  style: TextStyle(
-                    color: primaryColor,
-                    fontSize: getProportionateScreenHeight(18),
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const SelectStation()));
+                child: isLoading
+                    ? const CustomProgressIndicator()
+                    : Text(
+                        'LOGIN',
+                        style: TextStyle(
+                          color: primaryColor,
+                          fontSize: getProportionateScreenHeight(18),
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                onPressed: () async {
+                  final phoneNumber = phoneNumberCtrl.text.trim();
+                  final code = codeCtrl.text.trim();
+                  if (phoneNumber.isEmpty || code.isEmpty) {
+                    context.showScaffoldAlert('All fields are required!');
+                    return;
+                  }
+
+                  final result = await context
+                      .read<LoginProvider>()
+                      .login(phoneNumber, code);
+
+                  result.fold((error) {
+                    context.showScaffoldAlert(error);
+                  }, (sucess) {
+                    context.showScaffoldAlert("Login Successful",
+                        isError: false);
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const SelectStation()));
+                  });
                 },
               )
             ],
